4bc5a8819193fda09d22f276a1fa8639d26d273f
ShreyaDhir <dhirshreya@gmail.com>
2021-03-29 00:45:22 +0530

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {MaterialCommunityIcons, AntDesign} from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
+      Vi
     </View>
   );
 }
